86376e0cc6f492c3797f40278cdce93f3ed2b8be
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2021-08-20 17:03:56 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+# This below is the promised upload and download speed that your internet provider has offered you
+PROMISED_DOWN = "Your Own Download Speed (write as integer) "
+PROMISED_UP = " Your Own Upload Speed(write as integer"
+# Download the chromedriver
+CHROME_DRIVER_PATH = "Your Personal Chromedriver path"
+TWITTER_EMAIL = "EMAIL ADDRESS"
+TWITTER_PASSWORD = "PASSWORD"
+
+
+class InternetSpeedTwitterBot:
+
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.down = 0
+        self.up = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+        go_button = self.driver.find_element_by_xpath("/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[1]")
+        go_button.click()
+
+        time.sleep(40)
+
+        self.down = self.driver.find_element_by_xpath(
+            '/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print(f"Down:{self.down}")
+        self.up = self.driver.find_element_by_xpath(
+            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+
+        print(f" Up:{self.up}")
+
+    def tweet_at_provider(self):
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element_by_name("session[username_or_email]")
+        password = self.driver.find_element_by_name("session[password]")
+
+        email.send_keys(TWITTER_EMAIL)
+        password.send_keys(TWITTER_PASSWORD)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
+
+        tweet = f"Hey @Rogers, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element_by_xpath(
+            '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot.get_internet_speed()
+bot.tweet_at_provider()
